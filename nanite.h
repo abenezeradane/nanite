f6cea38763d795c6011ed1f0895d1611af9a4f71
PB020 <abe05081995@gmail.com>
2022-08-27 19:00:39 -0400

    Implemented basic Application structure

--- nanite.h
+++ nanite.h
@@ -23,3 +23,98 @@ void error(char* msg) {
 }
 
 #endif // MISCELLANEOUS_H
+
+#ifdef NANITE_IMPLEMENTATION
+  #ifndef NANITE_INPUT_IMPLEMENTATION
+  #define NANITE_INPUT_IMPLEMENTATION
+  #endif
+
+  #ifndef NANITE_APPLICATION_IMPLEMENTATION
+  #define NANITE_APPLICATION_IMPLEMENTATION
+  #endif
+#endif // NANITE_IMPLEMENTATION
+
+#ifndef NANITE_APPLICATION_INCLUDE
+#define NANITE_APPLICATION_INCLUDE
+
+/**
+ * @brief The application structure.
+ */
+typedef struct Application {
+  char* title;            // The title of the application.
+  SDL_Window* window;     // The SDL window.
+  SDL_GLContext context;  // The SDL OpenGL context.
+
+  int x, y;               // The x and y position of the window.
+  int width, height;      // The width and height of the window.
+
+  bool running;           // Whether the application is running.
+  bool fullscreen, vsync; // Whether the window is fullscreen and vsync is enabled.
+  
+  void (*load)(void);     // The function to call when the application is loaded.
+  void (*step)(void);     // The function to call when the application is stepped.
+} Application;
+
+/**
+ * @brief Initializes the application.
+ * 
+ * @param app The application to initialize.
+ */
+void run(Application* app);
+
+/**
+ * @brief Frees the application.
+ * 
+ * @param app The application to free.
+ */
+void close(Application* app);
+
+#endif // NANITE_APPLICATION_INCLUDE
+
+#ifdef NANITE_APPLICATION_IMPLEMENTATION
+#undef NANITE_APPLICATION_IMPLEMENTATION
+
+/**
+ * @brief Initializes the application.
+ * 
+ * @param app The application to initialize.
+ */
+void run(Application* app) {
+  // Check if the application is null.
+  if (!app)
+    error("Application is null.");
+
+  printf("\x1b[5m\x1B[32mInitializing\x1B[0m: \"%s\"\n", app -> title);
+
+  // ...
+
+  printf("\x1b[5m\x1B[32mInitialized\x1B[0m: \"%s\"\n", app -> title);
+
+  // ...
+
+  printf("\x1b[5m\x1B[32mRunning\x1B[0m: \"%s\"\n", app -> title);
+
+  // ...
+
+  close(app);
+}
+
+/**
+ * @brief Frees the application.
+ * 
+ * @param app The application to free.
+ */
+void close(Application* app) {
+  // Check if the application is null.
+  if (!app)
+    error("Application is null.");
+
+  printf("\x1b[5m\x1B[32mClosing\x1B[0m: \"%s\"\n", app -> title);
+
+  // ...
+
+  printf("\x1b[5m\x1B[32mClosed\x1B[0m: \"%s\"\n", app -> title);
+  // exit(EXIT_SUCCESS);
+}
+
+#endif // NANITE_APPLICATION_IMPLEMENTATION
